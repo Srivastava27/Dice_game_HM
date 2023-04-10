36295c376128e20c1cd21e98d0f32691098acd81
Srivastava27 <vanshikas2623@gmail.com>
2023-04-10 17:08:34 +0530

    roll function completed successfully.

--- script.js
+++ script.js
@@ -1,2 +1,51 @@
-console.log("Hello world")  //check for git connect
-console.log("repo added")  //repository added
+let btns=document.querySelectorAll(".dice");
+// console.log(btns);
+
+for(let i of btns){
+    i.addEventListener("click",roll);
+}
+
+// function dis(clk){
+//     clk.disabled=true;
+// }
+
+
+
+let spans=document.querySelectorAll("span");
+// console.log(spans[3]);
+
+function roll(event){
+    
+    let active="";
+    active=active+event.target.id;
+    // console.log(active);
+
+
+    let rand=parseInt(Math.random()*7);
+    // console.log(rand);
+
+
+    for(let i=1;i<spans.length;i++){
+        if(active.toUpperCase()==spans[i].id){
+           spans[i].innerHTML=rand;
+
+       } 
+    }
+}
+
+
+    let end_btn=document.getElementById("endButton");
+    console.log(end_btn);
+
+    end_btn.addEventListener("click",end);
+
+    function end(){
+        let max=0;
+        for(let i=1;i<spans.length;i++){
+            if(spans[i].innerHTML>max){
+                max=spans[i].innerHTML;
+            }
+        }
+        console.log("Winner",max)
+    }
+
